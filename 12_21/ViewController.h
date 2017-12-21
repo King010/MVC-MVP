e31a39dea3950adf5de342b9a34d7cb235364622
Zhang Tengfei <tengfei.zhang@renren-inc.com>
2017-12-21 11:48:46 +0800

    Initial Commit

---
+++ 12_21/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  12_21
+//
+//  Created by renren-mac on 2017/12/21.
+//  Copyright © 2017年 renren-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 12_21/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  12_21
+//
+//  Created by renren-mac on 2017/12/21.
+//  Copyright © 2017年 renren-mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
